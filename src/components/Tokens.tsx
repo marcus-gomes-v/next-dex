104382e2f425ce60eece1340fad1320aeb840501
Marcus Gomes <viniciusllgomes@gmail.com>
2025-01-06 21:40:44 +0100

    chore: remove old line

--- src/components/Tokens.tsx
+++ src/components/Tokens.tsx
@@ -55,6 +55,7 @@ export default function Tokens({
         </div>
       </div>
       <div className="mt-8 flow-root px-4 sm:px-6 lg:px-8 bg-gradient-to-b from-orange-900 to-orange-700 rounded-xl">
+        
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle">
             <table className="min-w-full divide-y divide-orange-700">
